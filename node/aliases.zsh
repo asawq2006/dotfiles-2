0e595652f981cc72ff88d6085a5f20488bf56cdd
gaboesquivel <contact@gaboesquivel.com>
2014-04-20 10:52:12 -0500

    aliases for installing gruntjs & gulpjs plugins in fewer characters

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -7,3 +7,16 @@ function gi() {
 function gci() {
   npm install --save-dev grunt-contrib-"$@"
 }
+
+
+# Install Grunt plugins and add them as `devDependencies` to `package.json`
+# Usage: `gi contrib-watch contrib-uglify zopfli`
+function gi() {
+        npm install --save-dev ${*/#/grunt-}
+}
+
+# Install Gulp plugins and add them as `devDependencies` to `package.json`
+# Usage: `gp uglify clean cache csso`
+function gp() {
+        npm install --save-dev ${*/#/gulp-}
+}
